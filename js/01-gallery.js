89d138e3bab4341bb360ee89bc00d700b7968f49
DanyloTytarenko <121917694+DanyloTytarenko@users.noreply.github.com>
2023-05-12 13:50:40 +0300

    fix

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,36 +1,46 @@
 import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-const gallery = document.querySelector(".gallery");
-const fragment = document.createDocumentFragment();
-galleryItems.forEach((item, index) => {
-  const image = document.createElement("li");
-  image.classList.add("gallery__item");
-  image.insertAdjacentHTML(
-    "afterbegin",
-    `<a class="gallery__link" href="#">
-    <img id=${index} class="gallery__image" src=${item.preview}  data-source=${item.original}" alt=${item.description}/>
-    </a>`
+const galleryBox = document.querySelector(".gallery");
+
+const createGalleryTable = () => {
+  const galleryTable = galleryItems
+    .map(
+      ({ preview, original, description }) =>
+        `<li class="gallery__item"><a class="gallery__link" href="${original}"><img class="gallery__image" src='${preview}' data-source='${original}' alt='${description}'></a></li>`
+    )
+    .join("");
+
+  return galleryTable;
+};
+
+galleryBox.insertAdjacentHTML("beforeend", createGalleryTable());
+
+galleryBox.addEventListener("click", galleryImgShow);
+
+function galleryImgShow(event) {
+  event.preventDefault();
+  if (event.target.dataset.source === undefined) {
+    return;
+  }
+  const instance = basicLightbox.create(
+    `<img src="${event.target.dataset.source}" >`,
+    {
+      onShow: (instance) => {
+        window.addEventListener("keydown", onEscButton);
+      },
+      onClose: (instance) => {
+        window.removeEventListener("keydown", onEscButton);
+      },
+    }
   );
-  fragment.appendChild(image);
-});
 
-gallery.append(fragment);
+  instance.show();
 
-const images = document.querySelectorAll(".gallery__item");
-images.forEach((item) => {
-  item.addEventListener("click", () => {
-    const instance = basicLightbox.create(`
-    <div class="modal">
-    <img src=${
-      galleryItems[item.firstElementChild.firstElementChild.id].original
-    } alt=${
-      galleryItems[item.firstElementChild.firstElementChild.id].original
-        .description
-    }/>
-    </div>
-`);
-
-    instance.show();
-  });
-});
+  function onEscButton(event) {
+    if (event.code === "Escape") {
+      console.log(event);
+      instance.close();
+    }
+  }
+}
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,28 +1,28 @@
 import { galleryItems } from "./gallery-items.js";
 // Change code below this line
-const gallery = document.querySelector(".gallery");
-const fragment = document.createDocumentFragment();
-galleryItems.forEach((item, index) => {
-  const image = document.createElement("li");
-  image.classList.add("gallery__item");
-  image.insertAdjacentHTML(
-    "afterbegin",
-    `<a class="gallery__link" href=${item.original}>
-    <img id=${index} class="gallery__image" src=${item.preview} alt=${item.description} title=${item.description} />
-    </a>`
-  );
-  fragment.appendChild(image);
+
+const galleryBox = document.querySelector(".gallery");
+
+const createGalleryTable = () => {
+  const galleryTable = galleryItems
+    .map(
+      ({ preview, original, description }) =>
+        `<li class="gallery__item"><a class="gallery__link" href="${original}"><img class="gallery__image" src="${preview}" alt="${description}" /></a></li>`
+    )
+    .join("");
+  return galleryTable;
+};
+
+galleryBox.insertAdjacentHTML("beforeend", createGalleryTable());
+
+galleryBox.addEventListener("click", galleryImgShow);
+
+var lightbox = new SimpleLightbox(".gallery a", {
+  captionsData: "alt",
+  captionDelay: 250,
 });
 
-gallery.append(fragment);
-
-const images = document.querySelectorAll(".gallery__item");
-let galleryBox = new SimpleLightbox(".gallery a", { captionDelay: 250 });
-
-images.addEventListener("click", () => {
-  galleryBox.on("show.simplelightbox", function () {
-    console.log(3);
-  });
-
-  galleryBox.on("error.simplelightbox", function (e) {});
-});
+function galleryImgShow(event) {
+  event.preventDefault();
+  lightbox;
+}
